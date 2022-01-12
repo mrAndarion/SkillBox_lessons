2ac048cfc270e96d143b04c28c820e5006a12836
ivany <ivany@playtika.com>
2022-01-12 02:36:26 +0100

    game done

--- Micarosppoftle_Game/Program.cs
+++ Micarosppoftle_Game/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace Micarosppoftle_Game
 {
@@ -16,12 +17,19 @@ namespace Micarosppoftle_Game
         private const string GameStartMessage = "Let the Game begins... ";
         private const string PlayerTurnMessage = "Player {0} turn: ";
         private const string CurrentNumberMessage = "Game number is {0}";
-        private const string WinnerMessage = "Player {0} is victorious";
+        private const string WinnerMessage = "Player {0} is victorious!!!";
         private const string RoundNumberMessage = "Round {0}: ";
         private const string RematchMessage = "Wanna rematch? (input yes or no) ";
+        private const string SelectDifficultyMessage = "Select AI difficulty (input easy or hard) ";
         private const string GameFinishedMessage = "Thank you for the game. Take care =) ";
-        
-        
+        private const string AiName = "Computer";
+        private const string EasyMode = "easy";
+        private const string HardMode = "hard";
+        private const string RematchYes = "yes";
+        private const string RematchNo = "no";
+        private const int MaxNameLength = 20;
+        private const int ComputerMoveDelay = 1500;
+
         public static void Main()
         {
             while (true)
@@ -29,7 +37,7 @@ namespace Micarosppoftle_Game
                 DisplayText(WelcomeMessage);
                 
                 DisplayText(PlayersNumberInputMessage);
-                var playersNumber = GetValidInt(Console.ReadLine(), 2, 8);
+                var playersNumber = GetValidInt(Console.ReadLine(), 1, 8);
                 var playersNames = GetPlayersNames(playersNumber);
 
                 DisplayText(GameNumberMinValueInputMessage);
@@ -40,8 +48,10 @@ namespace Micarosppoftle_Game
 
                 DisplayText(UserTryMaxValueInputMessage);
                 var userTry = GetValidInt(Console.ReadLine(), 1, int.MaxValue);
-
-                var restart = CoopGame(playersNames, gameNumber, userTry);
+                
+                var restart = playersNumber == 1 ? 
+                    SoloGame(playersNames[0], gameNumber, userTry, SelectDifficulty()) 
+                    : CoopGame(playersNames, gameNumber, userTry);
                 
                 if (restart)
                 {
@@ -58,16 +68,13 @@ namespace Micarosppoftle_Game
             DisplayText(GameStartMessage);
             var winnerName = "";
             var roundNumber = 1;
+            
             do
             {
                 DisplayText(string.Format(RoundNumberMessage, roundNumber));
                 foreach (var playerName in playersNames)
                 {
-                    DisplayText(string.Format(CurrentNumberMessage, gameNumber));
-                    DisplayText(string.Format(PlayerTurnMessage, playerName));
-                    var userTurn = GetValidInt(Console.ReadLine(), 1, 
-                        gameNumber < userTry ? gameNumber : userTry);
-                    gameNumber -= userTurn;
+                    PlayerMove(ref gameNumber, playerName, ref userTry);
                     if (gameNumber == 0)
                     {
                         winnerName = playerName;
@@ -81,6 +88,86 @@ namespace Micarosppoftle_Game
             DisplayText(string.Format(WinnerMessage, winnerName));
             return DoRematch();
         }
+        
+        private static bool SoloGame(string playerName, int gameNumber, int userTry, string gameMode)
+        {
+            DisplayText(GameStartMessage);
+            string winnerName;
+            var roundNumber = 1;
+            
+            do
+            {
+                DisplayText(string.Format(RoundNumberMessage, roundNumber));
+                PlayerMove(ref gameNumber, playerName, ref userTry);
+                if (gameNumber == 0)
+                {
+                    winnerName = playerName;
+                    break;
+                }
+
+                if (gameMode == EasyMode)
+                {
+                    PcMoveEasy(ref gameNumber, AiName, ref userTry);
+                }
+                else
+                {
+                    PcMoveHard(ref gameNumber, AiName, ref userTry);
+                }
+                
+                if (gameNumber == 0)
+                {
+                    winnerName = AiName;
+                    break;
+                }
+                roundNumber++;
+            } while (true);
+            
+            DisplayText(string.Format(WinnerMessage, winnerName));
+            return DoRematch();
+        }
+
+        private static void PlayerMove(ref int gameNumber, string playerName, ref int userTry)
+        {
+            DisplayText(string.Format(CurrentNumberMessage, gameNumber));
+            DisplayTurn(string.Format(PlayerTurnMessage, playerName));
+            var userTurn = GetValidInt(Console.ReadLine(), 1, 
+                gameNumber < userTry ? gameNumber : userTry);
+            EmptyString();
+            gameNumber -= userTurn;
+        }
+        
+        private static void PcMoveEasy(ref int gameNumber, string playerName, ref int userTry)
+        {
+            DisplayText(string.Format(CurrentNumberMessage, gameNumber));
+            DisplayTurn(string.Format(PlayerTurnMessage, playerName));
+            var randomGenerator = new Random();
+            var pcTurn = gameNumber < userTry ? randomGenerator.Next(1, gameNumber + 1) 
+                : randomGenerator.Next(1, userTry + 1);
+            Thread.Sleep(ComputerMoveDelay);
+            DisplayTurn(pcTurn.ToString());
+            EmptyString();
+            gameNumber -= pcTurn;
+        }
+        
+        private static void PcMoveHard(ref int gameNumber, string playerName, ref int userTry)
+        {
+            DisplayText(string.Format(CurrentNumberMessage, gameNumber));
+            DisplayTurn(string.Format(PlayerTurnMessage, playerName));
+            var randomGenerator = new Random();
+            int pcTurn;
+            if (gameNumber > userTry && gameNumber <= 2 * userTry && gameNumber != userTry + 1)
+            {
+                pcTurn = gameNumber - userTry - 1;
+            }
+            else
+            {
+                pcTurn = gameNumber <= userTry ? gameNumber : randomGenerator.Next(1, userTry + 1);
+            }
+            Thread.Sleep(ComputerMoveDelay);
+            DisplayTurn(pcTurn.ToString());
+            EmptyString();
+            gameNumber -= pcTurn;
+        }
 
         private static bool DoRematch()
         {
@@ -90,9 +177,22 @@ namespace Micarosppoftle_Game
                 DisplayText(RematchMessage);
                 inputString = Console.ReadLine();
 
-            } while (string.IsNullOrEmpty(inputString) || inputString != "yes" && inputString != "no");
+            } while (string.IsNullOrEmpty(inputString) || inputString != RematchYes && inputString != RematchNo);
 
-            return inputString == "yes";
+            return inputString == RematchYes;
+        }
+        
+        private static string SelectDifficulty()
+        {
+            string inputString;
+            do
+            {
+                DisplayText(SelectDifficultyMessage);
+                inputString = Console.ReadLine();
+
+            } while (string.IsNullOrEmpty(inputString) || inputString != EasyMode && inputString != HardMode);
+
+            return inputString;
         }
 
         private static int RandomizeGameNumber(int gameNumberMinValue, int gameNumberMaxValue)
@@ -106,8 +206,11 @@ namespace Micarosppoftle_Game
             var playersNames = new string[playersNumber];
             for (var i = 0; i < playersNumber; i++)
             {
-                DisplayText(string.Format(PlayerNameInputMessage, i+1));
-                playersNames[i] = Console.ReadLine();
+                do
+                {
+                    DisplayText(string.Format(PlayerNameInputMessage, i + 1));
+                    playersNames[i] = Console.ReadLine();
+                } while (string.IsNullOrEmpty(playersNames[i]) || playersNames[i].Length >= MaxNameLength);
             }
 
             return playersNames;
@@ -127,6 +230,16 @@ namespace Micarosppoftle_Game
         private static void DisplayText(string text)
         {
             Console.WriteLine(text);
+        }
+        
+        private static void DisplayTurn(string text)
+        {
+            Console.Write(text);
+        }
+        
+        private static void EmptyString()
+        {
+            Console.WriteLine();
         }
     }
 }
